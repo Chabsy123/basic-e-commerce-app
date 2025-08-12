2efdda437d4087363c9f1aad5ba9a94544938f18
Chukwu Sydney <chukwusydney061@gmail.com>
2025-08-12 08:42:31 +0100

    Basic Style with Tailwind CSS

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,14 +3,15 @@ import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  template: `<p>{{ title() }}</p> `,
+  // encapsulate styles which cannot be used outside the component
+  template: `<div class="bg-slate-100 px-4 py-3 shadow-md"><span>My Store</span></div> `,
     //  changing the content here will also update the header in the UI
   styles: `
-    
+
   `,
 })
 export class HeaderComponent {
-  title = signal('Basic Ecommerce App');
+
   // signals are better for dynamic data and when calling call like a function
   // we can use interpolation here to display the title but its better to use signal in todays code cause its highly reactive keeping track of where its used.
   // you can declare signal
